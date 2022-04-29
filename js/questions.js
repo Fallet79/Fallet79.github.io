5c1daf475137a87db33cc5c2065d499c4ca53c76
Fallet79 <104609311+Fallet79@users.noreply.github.com>
2022-04-28 22:44:59 -0600

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,96 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is most dangerous if contracted the COVID-19 virus?",
+    answer: "b.	Older people – especially these aged 70 and above",
+   options: [
+      "a.	Children",
+	  "b.	Older people – especially these aged 70 and above",
+	  "c.	People with no health conditions",
+	  "d.	European people"
+
+    ]
+  },
+    {
+    numb: 2,
+    question: "When should a face mask be worn?",
+    answer: "c. All of the above",
+    options: [
+	"a. On Public transport",
+	"b. In confined or crowded spaces",
+	"c. All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is an example of physical distance?",
+    answer: "a. Stop going to crowded places and visiting people house",
+    options: [
+	"a. Stop going to crowded places and visiting people house",
+	"b.	Stop talking to the people you live with",
+	"c.	Stop speaking to your friends on the phone"
+
+    ]
+  },
+    {
+    numb: 4,
+    question: "Can COVID-19 be cured?",
+    answer: "c.	No – but most people get better by themselves",
+    options: [
+	"a.	Yes – Hot drinks can cure COVID-19",
+	"b.	No – COVID-19 is a death sentence",
+	"c.	No – but most people get better by themselves"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How can people living with HIV/Diabetic protect themselves from COVID-19?",
+    answer: "d.	All of the above",
+    options: [
+    "a. Wash your hands regularly and follow the physical distance",
+	"b.	Keep taking their antiretroviral treatment and Exercise regularly, eat well and look after their health",
+	"d.	All of the above"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Does washing your hands prevent you from catching COVID?",
+    answer: "b.	Yes – normal soap and water on hand sanitizer is enough",
+    options: [
+	"a.	Yes - but only if you use a strong bleach",
+	"b.	Yes – normal soap and water on hand sanitizer is enough",
+	"c.	No – Washing your hands doesn’t stop COVID-19"
+    ]
+  },
+  {
+    numb: 7,
+    question: "How is COVID-19 passed on?",
+    answer: "a.	Through droplets that come from your mouth and nose when you cough or breathe out",
+    options: [
+	"a.	Through droplets that come from your mouth and nose when you cough or breathe out",
+	"b.	In sexual fluids, including semen, vaginal fluids",
+	"d.	All of the above"
+	]
+  },  
+  {
+    numb: 8,
+    question: "What are the common symptoms of COVID-19?",
+    answer: "d.	All of the above",
+    options: [
+	"a.	A new and continuous cough",
+	"b.	Fever and Tiredness",
+	"d.	All of the above"
+	]
+  },
+  {
+    numb: 9,
+    question: "Can you always tell if someone has COVID-19?",
+    answer: "a.	No- not everyone with COVID-19 has symptoms",
+    options: [
+	"a.	No- not everyone with COVID-19 has symptoms",
+	"b.	Yes- it will be obvious as a person with COVID has a lot of coughs",
+	"c.	Yes-you can tell by where a person comes from"
+	]
+  },
+];
